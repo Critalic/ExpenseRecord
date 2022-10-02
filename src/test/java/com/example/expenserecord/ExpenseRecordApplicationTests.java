5e437cd15466761a267a93cbdf0498ab0ae1a32d
V1tali1ty <vitalikkryvonosiuk@gmail.com>
2022-10-02 15:51:25 +0300

    Initial commit

---
+++ src/test/java/com/example/expenserecord/ExpenseRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenserecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
